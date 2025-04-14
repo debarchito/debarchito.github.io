beb9442f916d8b1d3763ea20682803b43074bb24
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2025-04-14 15:30:24 +0530

    deploy to github actions

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: "https://debarchito.is-a.dev",
+  base: "website",
   integrations: [svelte(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
