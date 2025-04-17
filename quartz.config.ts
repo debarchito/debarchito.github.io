c57d5193b38992a33a260412b05d25653965903f
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2025-04-17 06:09:55 +0000

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "debarchito.is-a.dev",
+    baseUrl: "debarchito.is-a.dev/ddg",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
