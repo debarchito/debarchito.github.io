422598ced95a95e27326176d7557f5c9ad498982
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2025-05-28 16:44:40 +0530

    Remove svelte

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,10 @@
-import svelte from "@astrojs/svelte";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://debarchito.is-a.dev",
-  integrations: [svelte(), sitemap()],
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
